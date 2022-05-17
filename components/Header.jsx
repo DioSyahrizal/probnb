bd6f47e8ff74c724c66db958d76720877cee83bf
Dio Syahrizal <diosyahrizal@gmail.com>
2022-05-17 16:16:37 +0700

    feat: add datepicker

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   GlobeAltIcon,
@@ -6,8 +6,24 @@ import {
   SearchIcon,
   UserCircleIcon,
 } from "@heroicons/react/solid";
+import { DateRangePicker } from "react-date-range";
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left */}
@@ -26,6 +42,8 @@ const Header = () => {
           className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
           type="text"
           placeholder="Start your search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer mx-auto md:mx-2" />
       </div>
@@ -38,6 +56,14 @@ const Header = () => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <DateRangePicker
+          ranges={[selectionRange]}
+          minDate={new Date()}
+          rangeColors={["#FD5B61"]}
+          onChange={handleSelect}
+        />
+      )}
     </header>
   );
 };
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
